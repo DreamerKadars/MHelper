7c70fc2acd9e0b3f143cfcd02225525dc06215ee
mashuo.dreamer <mashuo.dreamer@bytedance.com>
2023-07-27 10:43:59 +0800

    fix:修复python会panic造成进程终止

--- python_helper/parse.py
+++ python_helper/parse.py
@@ -52,42 +52,48 @@ model = YOLO('./best.pt')
 
 class ImageHandler(FileSystemEventHandler):
     def on_created(self, event): 
-        if not event.is_directory and event.src_path.lower().endswith((".jpg", ".png", ".jpeg")):
-            image_path = event.src_path
-            json_file = f"{os.path.splitext(os.path.basename(image_path))[0]}.json"
-            json_path = os.path.join(json_dir, json_file)
-        else :
-            return
+        try:
+            if not event.is_directory and event.src_path.lower().endswith((".jpg", ".png", ".jpeg")):
+                image_path = event.src_path
+                json_file = f"{os.path.splitext(os.path.basename(image_path))[0]}.json"
+                json_path = os.path.join(json_dir, json_file)
+            else :
+                return
 
-        resultsJson = []
-        print("开始预测")
-        results = model.predict(
-            event.src_path, save=False, imgsz=2048, conf=0.5, iou=0.2)
-        print("预测完成")
+            resultsJson = []
+            print("开始预测")
+            results = model.predict(
+                event.src_path, save=False, imgsz=2048, conf=0.5, iou=0.2)
+            print("预测完成")
 
-        result = results[0]
-        image_result = cv2.imread(event.src_path)
-        # 读取检测Box
-        for i in range(len(result)):
-            x1, y1, x2, y2 = result[i].boxes.xyxy[0, 0].item(), result[i].boxes.xyxy[0, 1].item(
-            ), result[i].boxes.xyxy[0, 2].item(), result[i].boxes.xyxy[0, 3].item()
-            resultsJson.append({"class": result[i].names[int(
-                result[i].boxes.cls[0])], "x1": x1, "y1": y1, "x2": x2, "y2": y2})
-            cv2.rectangle(image_result, (int(x1), int(y1)), (int(
-                x2), int(y2)), get_color(int(result[i].boxes.cls[0])), 2)
+            result = results[0]
+            image_result = cv2.imread(event.src_path)
+            # 读取检测Box
+            for i in range(len(result)):
+                x1, y1, x2, y2 = result[i].boxes.xyxy[0, 0].item(), result[i].boxes.xyxy[0, 1].item(
+                ), result[i].boxes.xyxy[0, 2].item(), result[i].boxes.xyxy[0, 3].item()
+                resultsJson.append({"class": result[i].names[int(
+                    result[i].boxes.cls[0])], "x1": x1, "y1": y1, "x2": x2, "y2": y2})
+                cv2.rectangle(image_result, (int(x1), int(y1)), (int(
+                    x2), int(y2)), get_color(int(result[i].boxes.cls[0])), 2)
 
-        cv2.imwrite(os.path.join(image_result_dir,os.path.basename(image_path)), image_result)
-        
-        img = cv2.imread(event.src_path)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, mask_all_192 = cv2.threshold(gray, 192, 255, cv2.THRESH_BINARY)
-        cv2.imwrite(os.path.join(image_process_dir_192,os.path.basename(image_path)), mask_all_192)
-        
-        ret, mask_all_127 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        cv2.imwrite(os.path.join(image_process_dir_127,os.path.basename(image_path)), mask_all_127)
-        # 将检测结果保存为JSON文件
-        with open(json_path, "w") as json_file:
-            json.dump(resultsJson, json_file)
+            cv2.imwrite(os.path.join(image_result_dir,os.path.basename(image_path)), image_result)
+            
+            img = cv2.imread(event.src_path)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret, mask_all_192 = cv2.threshold(gray, 192, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(os.path.join(image_process_dir_192,os.path.basename(image_path)), mask_all_192)
+            
+            ret, mask_all_127 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(os.path.join(image_process_dir_127,os.path.basename(image_path)), mask_all_127)
+            # 将检测结果保存为JSON文件
+            with open(json_path, "w") as json_file:
+                json.dump(resultsJson, json_file)
+        except Exception as e:
+            # 处理异常
+            print("发生异常:", e)
+        else:
+            print("没有发生异常，完成了一次预测")
 
 def main():
     event_handler = ImageHandler()
